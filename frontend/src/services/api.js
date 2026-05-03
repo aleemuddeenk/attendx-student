a30fcf387e64b3f703a8cba671aee2dbc96b7ab2
Aleemuddeen <aleemuddeenss@gmail.com>
2026-05-03 16:04:15 +0530

    fix: update backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Spring Boot default
+  baseURL: "https://attendx-student-production.up.railway.app", // Spring Boot default
 });
 
 // Automatically inject JWT token into all requests if it exists in local storage
